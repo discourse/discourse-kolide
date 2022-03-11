7e30d130991f1f98d9333165ed0bd2e159e4ba42
Vinoth Kannan <svkn.87@gmail.com>
2022-03-11 18:41:54 +0530

    FEATURE: update user alert PM content after a device is destroyed. (#40)
    Previously, the PM is not updated automatically. So it confuses the end-users.

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -23,4 +23,19 @@ describe ::Kolide::Device do
     expect { issue.reload }.to raise_error(ActiveRecord::RecordNotFound)
   end
 
+  describe "#destroy" do
+    it "updates user alert PM" do
+      device = Fabricate(:kolide_device)
+      issue = Fabricate(:kolide_issue, device: device)
+      ::Kolide::UserAlert.new(device.user).remind!
+
+      post = Topic.last.first_post
+      expect(post.raw).to include("Screen Lock Disabled")
+
+      device.destroy
+
+      expect(post.reload.raw).to include(I18n.t("kolide.group_alert.no_issues"))
+    end
+  end
+
 end

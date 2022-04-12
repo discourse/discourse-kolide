31c2eb64e1732a9073efc5aa4780c72ccd4af249
Vinoth Kannan <svkn.87@gmail.com>
2022-04-12 13:34:24 +0530

    FIX: pluck device ids before performing updating the db. (#43)
    After updating the records `pluck` method won't return the device id of the records which are affected by the `update_all` method.

---
+++ spec/kolide_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative 'spec_helper'
+
+describe Kolide do
+  include_context "spec helper"
+
+  let(:user) { Fabricate(:user) }
+  let(:device) { Fabricate(:kolide_device, user: user, user_id: user.id, uid: 12345) }
+  let(:issue) { Fabricate(:kolide_issue, device: device) }
+
+  def stub_api(type)
+    content = { status: 200, headers: { "Content-Type" => "application/json" } }
+    url = "#{::Kolide::Api::BASE_URL}#{type == :issues ? "issues/open" : type}?per_page=500"
+    content = content.merge(body: get_kolide_response("#{type}.json"))
+    stub_request(:get, url).to_return(content)
+  end
+
+  before do
+    user.custom_fields["kolide_person_id"] = "23456"
+    user.save_custom_fields
+
+    stub_api(:devices)
+    stub_api(:checks)
+    stub_api(:issues)
+  end
+
+  describe "sync!" do
+    it "updates alert post after issues resolved" do
+      issue
+      ::Kolide::UserAlert.new(user)
+
+      post = Topic.private_messages_for_user(user).last.first_post
+      expect(post.raw).to include("[^#{issue.id}]: user: deviceuser")
+
+      ::Kolide.sync!
+
+      expect(issue.reload.resolved).to be_truthy
+      expect(post.reload.raw).not_to include("[^#{issue.id}]: user: deviceuser")
+    end
+  end
+end

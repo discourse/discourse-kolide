191e10490ebb591f7ab629a7dfe4ab6cc8e629f4
Vinoth Kannan <svkn.87@gmail.com>
2023-10-01 19:11:49 +0530

    DEV: check old IPs with Kolide to determine onboarded devices.
    Previously, we only checked the current IP address of the user to determine whether a device is onboarded or not. It creates problem when the IP address is not updated either in Kolide system or in Discourse. So, now we're including the old IP addresses from the `UserAuthTokenLog` table. Also, we're now creating a cookie to prevent repeated checking.

---
+++ lib/user_extension.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Kolide::UserExtension
+  def self.prepended(base)
+    base.has_many :kolide_devices, class_name: "::Kolide::Device"
+
+    base.class_eval do
+      def self.find_by_kolide_json(data)
+        return if data.blank?
+
+        custom_field =
+          UserCustomField.find_or_initialize_by(name: "kolide_person_id", value: data["id"])
+        return custom_field.user unless custom_field.new_record?
+
+        email = data["email"]
+        user = User.find_by_email(email)
+
+        if user.blank?
+          Rails.logger.warn("Unable find the Discourse user for email address '#{email}'")
+          return
+        end
+
+        custom_field.user_id = user.id
+        custom_field.save!
+
+        user
+      end
+    end
+  end
+end

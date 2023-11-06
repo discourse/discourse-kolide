35eb2abfbb844a9a99ab42cbfaa77518496daca6
Vinoth Kannan <svkn.87@gmail.com>
2023-11-06 23:06:15 +0530

    DEV: remove Kolde onboarding enforcement feature.

--- lib/application_controller_extension.rb
+++ lib/application_controller_extension.rb
@@ -17,31 +17,10 @@ module Kolide::ApplicationControllerExtension
       if device_id.present? 
         if ::Kolide::Device.where(id: device_id.to_i, user_id: [nil, current_user.id]).exists?
           cookies.delete(:kolide_non_onboarded)
-          return
         else
           cookies.delete(:kolide_device_id)
         end
       end
-
-      enforce_at = cookies[:kolide_non_onboarded]
-
-      if enforce_at.present?
-        enforce_at = Time.at(enforce_at.to_i)
-      else
-        enforce_at = SiteSetting.kolide_onboarding_grace_period_days.days.from_now
-        cookies[:kolide_non_onboarded] = {
-          value: enforce_at.to_i,
-          expires: 1.year.from_now
-        }
-      end
-
-      return if enforce_at.future?
-
-      redirect_path = path("/u/#{current_user.encoded_username}/preferences/kolide")
-      return if request.fullpath.start_with?(redirect_path)
-
-      redirect_to path(redirect_path)
-      nil
     end
   end
 end
